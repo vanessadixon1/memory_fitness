e1e9c120b5d568bceace1cab9c3cb6db4beb3767
vanessadixon1 <mclendonfamily88@gmail.com>
2024-01-20 10:40:33 -0500

    refactored code

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # memory_fitness
 
-Memory Fitness is a simple JS Web Application, users have 30 seconds to match blocks after 30 seconds the game will end.
+Memory Fitness is a simple JS Web Application, users have 30 seconds to match the same colors after 30 seconds the game will end and the results will be displayed in an alert.
 
-![Model](snippet.png)
-![Model](snippt2.png)
+![Model](memorygame.png)
+![Model](memory.png)
 
 ### How to Play
-* select whether you would like to play
-* Game clock starts, user has 15 seconds to complete the game by matching block colors
+* Click start button to start the game
+* Game clock starts, user has 30 seconds to complete the game by matching colors
+* User will get an alert when there a match has been located
 * User as the ability to end the game and restart
+* Once the game is over no more, the user will not be able to click on any blocks
 
 ### Technologies used:
 * HTML
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const gameContainer = document.getElementById("game");
-let clicked = 2;
-let pick1 = "";
-let target1 = "";
-let divCount;
-let arr = [];
-let score = 2;
-let timer = 0;
-let timerId;
+const startGame = document.querySelector("#startGame button");
+const removeText = document.querySelector("#startGame");
+const timeLeft = document.querySelector("span");
 
+let isActive;
+let score = 0;
+let gameOver;
+let colorDivs;
+let clickedColors = [];
 
 const COLORS = [
   "red",
@@ -22,210 +22,112 @@ const COLORS = [
   "purple"
 ];
 
-let replayButton = document.getElementById("replayButton");
-replayButton.classList.add("display");
+startGame.addEventListener('click', function() {
+  removeText.remove();
+  let time = 30;
+  timeLeft.innerText = time;
+  
+  let timer = setInterval(() => {
+    if(time <= 0) {
+      clearInterval(timer);
+      alert(`Game Over! Your Score: ${score}`);
+      isActive = false;
+      toggleEventOnAndOff(isActive,colorDivs);
 
-function shuffle(array) {
-  let counter = array.length;
-
-  while (counter > 0) {
-    let index = Math.floor(Math.random() * counter);
-    counter--;
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
-  return array;
-}
-
-let shuffledColors = shuffle(COLORS);
-
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    const newDiv = document.createElement("div");
-
-    newDiv.classList.add(color);
-
-    newDiv.addEventListener("click", handleCardClick);
-
-    gameContainer.append(newDiv);
-
-    divCount = document.querySelectorAll("#game div");
-  }
-}
-
-function handleCardClick(event) {
-
-  if(clicked > 0 && score < 12) {
-      event.target.style.backgroundColor = event.target.className;
-       if(clicked === 2) {
-        pick1 = event.target;
+      if(!localStorage.getItem("gameScore")) {
+        localStorage.setItem('gameScore', JSON.stringify(score));
       }else {
-        if(pick1.className !== event.target.className) {
-        
-          setTimeout(() => {
-            pick1.style.backgroundColor = "";
-            event.target.style.backgroundColor = "";
-            cleanup();
-          },100);
-          return;
-      }else if(pick1.className === event.target.className) {
-        pick1.removeEventListener("click", handleCardClick);
-        event.target.removeEventListener("click", handleCardClick);
-        score+=2;
-        cleanup();
-        return;
+        let storedScore = Number(localStorage.getItem("gameScore"));
+        if(score > storedScore) {
+          localStorage.setItem('gameScore', JSON.stringify(score));
+        }
+      }
+      
+    }
+    timeLeft.innerText = time;
+    time--;
+
+  },1000)
+
+  function shuffle(array) {
+    let counter = array.length;
+
+    while (counter > 0) {
+      let index = Math.floor(Math.random() * counter);
+      counter--;
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+    return array;
+  }
+
+  let shuffledColors = shuffle(COLORS);
+
+  function createDivsForColors(colorArray) {
+    for (let color of colorArray) {
+      const newDiv = document.createElement("div");
+
+      newDiv.classList.add(color, "colors");
+    
+      newDiv.addEventListener("click", handleCardClick);
+
+      gameContainer.append(newDiv);
+    }
+  }
+
+  function handleCardClick(event) {
+    let color = event.target.className.trim();
+    colorDivs = document.querySelectorAll(".colors");
+    
+    event.target.style.backgroundColor = color.split(" ")[0];
+    clickedColors.push(event.target)
+    event.target.removeEventListener('click',handleCardClick);
+
+    if(clickedColors.length === 2) {
+      isActive = false;
+
+      toggleEventOnAndOff(isActive, colorDivs);
+
+      let matched = clickedColors[0].className === clickedColors[1].className
+      if(!matched) {
+        setTimeout(() => {
+          for(let i of clickedColors) {
+            i.style.backgroundColor = "white";
+          }
+          isActive = true;
+          resetGame(isActive, colorDivs);
+        },1000);
+      } else {
+        setTimeout(() => {
+          alert("Matched");
+          score++;
+        },100);
+        isActive = true;
+        resetGame(isActive,colorDivs);
       }
     }
-    clicked--;
+
   }
-  console.log(score)
-}
 
-function cleanup() {
-  clicked = 2;
-  pick1 = "";
-  
-}
+  createDivsForColors(shuffledColors);
 
-function startGame() {
-  let value;
-  let answerObj;
-  let idAndNameVal;
-  let h2 = document.createElement("h3");
-  h2.innerText = "Would you like to play? ";
-  let yesInput = document.createElement("input");
-  let noInput = document.createElement("input");
-  const yesLabel = createNewLabel("Yes");
-  const noLabel = createNewLabel("No");
-  let innerDiv = document.createElement("div");
-
-  if(yesInput) {
-    value = "Yes";
-    idAndNameVal = "yesAnswer";
-  }else {
-    value = "No";
-    idAndNameVal = "noAnswer";
+  function resetGame(isActive, colorDivs) {
+      countClick = 0;
+      clickedColors = [];
+      toggleEventOnAndOff(isActive, colorDivs);
   }
-  
-  answerObj = {type: "checkbox", id:[idAndNameVal], name:[idAndNameVal], value: [value]};
 
-  setAttributes(yesInput, answerObj);
-  setAttributes(noInput, answerObj);
-
-  innerDiv.setAttribute("id","innerDiv")
-  innerDiv.append(h2,yesInput, yesLabel, noInput, noLabel);
- 
-  gameContainer.append(innerDiv);
-
-}
-
-function setAttributes(element, attributes) {
-  for(let attribute in attributes) {
-    element.setAttribute(attribute, attributes[attribute])
-  }
-}
-
-//create new Label
-function createNewLabel(value) {
-  let label = document.createElement("label");
-  label.innerText = value;
-  setAttributes(label,{for: value})
-  return label;
-}
-
-startGame();
-
-const input = document.getElementById("yesAnswer");
-
-let innerDiv = document.getElementById("innerDiv");
-
-let h2 = document.createElement("h2");
-
-input.addEventListener("click", (evt) => {
-  
-  let clockTime = document.querySelector(".clock_time");
-  let checked = evt.target.checked;
-  let newMsg;
-    console.dir(checked)
-    if(checked) {
-      innerDiv.remove();
-      h2.innerText = "Game Loading..."
-      gameContainer.append(h2);
-
-      timerId = setInterval(() => {
-        clockTime.innerText = timer +=1;
-       
-        if(timer === 15 && score < 10) {
-          replayButton.classList.remove("display");
-          newMsg = `Your time has expired!\nScore:`;
-          clearTimer(timerId, newMsg);
-        }else if(score === 12) {
-          replayButton.classList.remove("display");
-          newMsg = `Congratulations You Won!\nScore:`;
-          clearTimer(timerId,newMsg);
-        }
-
-      },1000);
-
-      setTimeout(() => {
-        h2.remove();
-        createDivsForColors(shuffledColors);
-        gameContainer.append(createNewButton("Restart"));
-        
-        let btn = document.getElementById('Restart');
-        btn.addEventListener("click", (evt) => {
-          restartOrEndGame();
-        })
-
-        let endButton = document.getElementById("endGame");
-
-        endButton.addEventListener("click",(evt) => {
-          clearTimer(timerId, "Game Over!");
-          replayButton.classList.remove("display");
-        });
-
-      },800);
-    }
-});
-
-
-function restartOrEndGame() {
-  let restartAnswer = prompt("Would you like to Restart the Game? Type 'Restart'");
-
-  if(restartAnswer.toLowerCase() === "restart" && (score < 12)) {
-    let answer = prompt(`are you sure you would like to ${restartAnswer} the game? Type 'Yes or No'`);
-
-    if(answer.toLowerCase() === "yes") {
-      let currScore = score === 2 ? 0 : score;
-      clearInterval(timerId);
-      alert(`Your final score is: ${currScore}`);
-      setTimeout(() => {
-        location.reload();
-      },300);
-    }else if(answer.toLowerCase() == "no") {
-      alert(`Game Continued...`);
-    }else {
-      alert("You can only enter Yes or No");
+  function toggleEventOnAndOff(isActive, colorDivs) {
+    if(!isActive) {
+      for(let i of colorDivs) {
+        i.removeEventListener("click",handleCardClick)
+      }
+    } else {
+      for(let i of colorDivs) {
+        i.addEventListener("click",handleCardClick)
+      }
     }
   }
-} 
 
-replayButton.addEventListener("click",(evt) => {
-  location.reload();
 })
-
-function createNewButton(value) {
-  let button;
-  button = document.createElement("button");
-  button.innerText = value;
-  button.setAttribute("id", value);
-  return button;
-}
-
-function clearTimer(timerId,msg) {
-  clearInterval(timerId);
-  gameContainer.classList.add("display");
-  h2.innerText = `${msg} ${score === 2 ? 0: score}`;
-  document.body.append(h2);
-}
